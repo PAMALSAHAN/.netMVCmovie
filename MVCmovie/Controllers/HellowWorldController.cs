15452e598c5d6ff4f6c3a9e8635e3ebe7c95f493
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-03-28 17:58:09 +0530

    conncting controller with view

---
+++ MVCmovie/Controllers/HellowWorldController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCmovie.Controllers
+{
+    public class HellowWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult pamal()
+        {
+            return View();
+        }
+        public IActionResult chamath()
+        {
+            return View();
+        }
+
+        public IActionResult welcome(string name,int num)
+        {
+            ViewData["message"] = "Hellow" + name;
+            ViewData["age"] = "My age is " + num;
+
+            return View();
+        }
+
+
+
+        //public string pamal()
+        //{
+        //    return "My name is pamal";
+        //}
+
+        public string sahan()
+        {
+            return "My name is sahan";
+        }
+
+        //public string chamath(string id ,string name)
+        //{
+        //    return "My age is "+id +"and my name is "+name;
+        //}
+
+    }
+}
